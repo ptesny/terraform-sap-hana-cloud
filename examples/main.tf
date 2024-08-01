95817929c8574893d5b1643a3ae6f83bfacf4c7a
ptesny <ptesny@wanadoo.fr>
2024-08-01 18:16:06 +0200

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -7,7 +7,7 @@ resource "btp_subaccount" "this" {
 module "hana_cloud" {
   source        = "ptesny/hana-cloud/sap"
   version       = "0.0.1"
-  instance_name = "hana-cloud"
+  instance_name = "hc-trial"
   admins        = var.admins
   subaccount_id = btp_subaccount.this.id
   whitelist_ips = ["0.0.0.0/0"]
