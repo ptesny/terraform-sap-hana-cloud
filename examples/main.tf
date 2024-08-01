0aecee4acbef8c98f5bca0181c44334b901a824c
ptesny <ptesny@wanadoo.fr>
2024-08-01 23:26:27 +0200

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -5,9 +5,17 @@ resource "btp_subaccount" "this" {
 }
 
 module "sap_hana_cloud" {
-  source        = "github.com/ptesny/terraform-sap-hana-cloud"
-  instance_name = "hc-trial"
-  admins        = var.admins
-  subaccount_id = btp_subaccount.this.id
-  whitelist_ips = ["0.0.0.0/0"]
+  source                     = "github.com/ptesny/terraform-sap-hana-cloud"
+  service_name               = "hana-cloud-trial"
+  plan_name                  = "hana"
+  hana_cloud_tools_app_name  = "hana-cloud-tools-trial"
+  hana_cloud_tools_plan_name = "tools"  
+
+  memory                     = 16
+  vcpu                       = 1
+
+  instance_name              = "hc-trial"
+  admins                     = var.admins
+  subaccount_id              = btp_subaccount.this.id
+  whitelist_ips              = ["0.0.0.0/0"]
 }
