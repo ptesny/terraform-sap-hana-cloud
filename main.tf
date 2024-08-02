17cf0a739177bf160306f54dfa7113f2c2f0e3b6
ptesny <ptesny@wanadoo.fr>
2024-08-02 20:10:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,11 +127,6 @@ resource "btp_subaccount_service_binding" "hc_binding_dbadmin" {
   ]
 }
 
-# look up service binding by name
-data "btp_subaccount_service_binding" "my_hana_service_binding" {
-  subaccount_id = var.subaccount_id
-  name          = "hc_binding_dbadmin"
-}
 
 # create a service binding in a subaccount
 resource "btp_subaccount_service_binding" "hc_binding" {
