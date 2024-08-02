0e175193a9b491306ac93438ecb3c8915d86d540
ptesny <ptesny@wanadoo.fr>
2024-08-02 19:08:24 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "dbadmin_password" {
-  value = jsondecode(btp_subaccount_service_binding.hc_binding_dbadmin.instances[0])
-}
-
 output "sap_hana_cloud_central" {
   value = btp_subaccount_subscription.hana_cloud_tools.subscription_url
 }
