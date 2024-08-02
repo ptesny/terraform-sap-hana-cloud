c7f81baca206b10b284f2660b562829beac36e8d
ptesny <ptesny@wanadoo.fr>
2024-08-02 20:02:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -128,7 +128,7 @@ resource "btp_subaccount_service_binding" "hc_binding_dbadmin" {
 }
 
 # look up service binding by name
-data "btp_subaccount_service_binding" "hc_binding_dbadmin_data" {
+data "btp_subaccount_service_binding" "hc_binding_dbadmin" {
   subaccount_id = var.subaccount_id
   name          = "hc_binding_dbadmin"
   depends_on = [
