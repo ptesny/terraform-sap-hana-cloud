ec5a4ccaab638e8b17835902832ff17b2b75e83b
ptesny <ptesny@wanadoo.fr>
2024-08-02 18:58:45 +0200

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "dbadmin_password" {
+  value = btp_subaccount_service_binding.hc_binding_dbadmin.password
+}
+
+output "sap_hana_cloud_central" {
+  value = btp_subaccount_subscription.hana_cloud_tools.subscription_url
+}
